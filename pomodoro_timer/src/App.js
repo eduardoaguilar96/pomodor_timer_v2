ee0246aab3c710e6b51e447ba34e8bc8fd828542
eduardoaguilar96 <eduaguilar96@gmail.com>
2022-10-27 16:07:35 +0200

    maquetacion

---
+++ pomodoro_timer/src/App.js
@@ -0,0 +1,33 @@
+
+import './App.css';
+import Button from 'react-bootstrap/Button';
+
+
+function App() {
+  return (
+    <div class="container" >
+<h1 class="titulo_app">POMODORO TIMER</h1>
+
+
+
+<div class="container-fluid">
+<h1 class="titulo">25:00</h1>
+
+ 
+
+
+
+    
+        
+    <Button variant="primary" size="lg">START</Button>{' '}
+    <Button variant="primary" size="lg">RESET</Button>{' '}
+
+    </div>
+      
+    
+  
+    </div>
+  );
+}
+
+export default App;
